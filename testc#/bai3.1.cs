eccb3831c8c5e3e2dc346d9fc7e9e3e48ef823fa
Chu Chấn Huy <codon2972@gmail.com>
2026-08-26 01:15:39 -0700

    Add files via upload
    may tinh bo tui

---
+++ testc#/bai3.1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Timers;
+class Program
+{
+    static void Main()
+    {
+        char op;
+        double a, b, kq;
+        Console.Write("nhap so thu nhat a: ");
+        a = double.Parse(Console.ReadLine());
+        Console.Write("nhap so thu hai b: ");
+        b = double.Parse(Console.ReadLine());
+        Console.WriteLine("chon phep tinh: + | - | * | / ");
+        op = char.Parse(Console.ReadLine());
+        switch (op)
+        {
+            case '+': kq = a + b; break;
+            case '-': kq = a - b; break;
+            case '*': kq = a * b; break;
+            case '/': kq = b != 0 ? a / b : 0; break;
+            default:
+                {
+                    Console.WriteLine("phep tinh ko hop le"); return;
+                 }
+        }
+        Console.WriteLine($"ket qua {a} {op} {b} = {kq}");
+    }
+}
